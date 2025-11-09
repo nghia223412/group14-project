48b10c0bc2c51028c08f730ae1607def4143f95d
Trần Minh Nghĩa <nghia223412@student.nctu.edu.vn>
2025-11-09 21:43:01 +0700

    Update UserList.jsx

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -10,7 +10,7 @@ function UserList() {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/api/users');
+      const response = await axios.get('https://group14-project-a92d.onrender.com/api/users');
       setUsers(response.data);
     } catch (error) {
       console.error('Error fetching users:', error);
