8a51a566aca743c73ee5aeb36bae76fc04df39c8
Trần Minh Nghĩa <nghia223412@student.nctu.edu.vn>
2025-11-09 20:45:32 +0700

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Middleware xác thực token
 exports.protect = async (req, res, next) => {
