f7f90a0798beebe91f154b3e217f9ee51a5fe0df
Trần Minh Nghĩa <nghia223412@student.nctu.edu.vn>
2025-11-09 21:42:42 +0700

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://group14-project-a92d.onrender.com/api';
 
 function Profile({ currentUser, token, onUpdateSuccess }) {
   const [isEditing, setIsEditing] = useState(false);
