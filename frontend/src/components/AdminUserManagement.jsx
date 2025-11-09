d3b31083e681d22c4c4bbf630c0b8365e4578b30
Trần Minh Nghĩa <nghia223412@student.nctu.edu.vn>
2025-11-09 21:41:41 +0700

    Update AdminUserManagement.jsx

--- frontend/src/components/AdminUserManagement.jsx
+++ frontend/src/components/AdminUserManagement.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react'; // THÊM useCallback
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://group14-project-a92d.onrender.com/api';
 
 function AdminUserManagement({ token, currentUser }) {
   const [users, setUsers] = useState([]);
