73429f61c4c13ac0b6861d11c91558b096e94ed9
Trần Minh Nghĩa <nghia223412@student.nctu.edu.vn>
2025-11-09 22:36:18 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://group14-project-virid.vercel.app',  // Giữ lại nếu cần
     'https://group14-project-virid2.vercel.app', // Giữ lại nếu cần
-    'https://group14-project-vdi2.vercel.app'
+    'https://group14-project-vdi2.vercel.app',
+    'https://group14-project-vdi2-f3hoo3j7g-nghias-projects-9d6cd8f7.vercel.app'
 ];
 
 const corsOptions = {
